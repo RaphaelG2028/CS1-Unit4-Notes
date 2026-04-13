df818de3aa109eda6f6e2ffc2c3e87188940021b
RaphaelG2028 <Raphael.goldstein2028@gbwl.org>
2026-04-13 12:46:55 +0000

    console log stuff

--- script.js
+++ script.js
@@ -3,3 +3,24 @@
 
 /* The JS code below is an example STATEMENT(instruction/command) every statement ends in a SEMICOLON */
 console.log("hello world");
+
+// DECLARE a new variable (make space)
+let username;
+
+// ASSIGN a value to a variable 
+username = "ms.walter13"; 
+
+// see the value stored in a variable 
+console.log(username);
+
+// DECLARE + ASSIGN in one line
+let pokemon = "pikachu"; 
+let luckyNum = 4; //no quotes around numbers!
+
+// look at our new variables 
+console.log(pokemon)
+console.log(luckyNum)
+
+// change the value stored in a var
+luckyNum = 15
+console.log(luckyNum)
